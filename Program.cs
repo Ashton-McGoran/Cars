488924288f32f964abdc7365215aa0563611f370
ashto <ashto@MCBUCKETS50>
2024-03-03 09:36:28 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+
+interface ICar
+{
+    // Properties
+    string Brand { get; }
+    string Model { get; }
+
+    // Method
+    string Describe();
+}
+
+
+class Car : ICar
+{
+   
+    public string Brand { get; }
+    public string Model { get; }
+
+    
+    public Car(string brand, string model)
+    {
+        Brand = brand;
+        Model = model;
+    }
+
+    // Interface method
+    public string Describe()
+    {
+        return $"Car - Brand: {Brand}, Model: {Model}";
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Prompt user to input information for a car
+        Console.WriteLine("Enter Car's Brand:");
+        string carBrand = Console.ReadLine();
+
+        Console.WriteLine("Enter Car's Model:");
+        string carModel = Console.ReadLine();
+
+        // Create a car object with user-provided information
+        Car car = new Car(carBrand, carModel);
+
+        // Display information
+        Console.WriteLine(car.Describe());
+    }
+}
